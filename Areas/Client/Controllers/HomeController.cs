ec49a6dcb6bf762427a5e09cfb528b2eee8852e5
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-02-04 22:16:16 +0800

    3. Created `Register` page Scaffholded

---
+++ Areas/Client/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asprule1020.Areas.Client.Controllers
+{
+    [Area("Client")]
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,8 +22,6 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{area=Client}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
