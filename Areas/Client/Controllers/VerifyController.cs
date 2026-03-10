35794d22740368348f4949b3ae0dfde353660ed9
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-03-10 20:16:07 +0800

    45. Verify Status Layout

---
+++ Areas/Client/Controllers/VerifyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asprule1020.Areas.Client.Controllers
+{
+    public class VerifyController : Controller
+    {
+        [Area("Client")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
