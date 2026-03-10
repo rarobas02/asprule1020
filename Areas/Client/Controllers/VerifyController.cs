5cd5814cb0e432df0a8101e7f57b2fd03688f21e
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-03-10 21:56:35 +0800

    46. Get Verification Details

--- Areas/Client/Controllers/VerifyController.cs
+++ Areas/Client/Controllers/VerifyController.cs
@@ -1,13 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using asprule1020.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace asprule1020.Areas.Client.Controllers
 {
+    [Area("Client")]
     public class VerifyController : Controller
     {
-        [Area("Client")]
+        private readonly IUnitOfWork _unitOfWork;
+
+        public VerifyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(string transId)
+        {
+            var normalizedTransId = transId?.Trim();
+
+            if (string.IsNullOrWhiteSpace(normalizedTransId))
+            {
+                ViewData["LookupMessage"] = "Transaction id does not found.";
+                return View(model: null);
+            }
+
+            var register = _unitOfWork.Register.Get(x => x.TransId == normalizedTransId);
+            if (register == null)
+            {
+                ViewData["LookupMessage"] = "Transaction id does not found.";
+                ViewData["SearchedTransId"] = normalizedTransId;
+                return View(model: null);
+            }
+
+            return View(register);
         }
     }
 }
